ac7aa6633e362366074cfe5863af61dfe0dcfda6
sfontichelli <79066356+sfontichelli@users.noreply.github.com>
2026-03-06 23:34:27 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-// Pegá acá la URL del Apps Script desplegado como Web App.
-// Si no la completás, el dashboard sigue funcionando en modo local.
-window.APP_CONFIG = {
-  API_URL: "PEGAR_AQUI_LA_URL_DEL_APPS_SCRIPT"
+{
+  API_URL: https://script.google.com/macros/s/AKfycbwzzhD18pyECMHnCpMUkbn2vv_7Vz-WRwDAH67gPJb-qtfgabdAKePeDEcEQRUTlaym/exec
 };
