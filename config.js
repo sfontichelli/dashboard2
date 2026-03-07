8ccd1cbec8e4b32bb1a08893652c3ca12760ff7b
sfontichelli <79066356+sfontichelli@users.noreply.github.com>
2026-03-06 23:49:20 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbwzzhD18pyECMHnCpMUknb2vv_7Vz-WRwDAH67gPJb-qtfgabdAKePeDEcEQRUTlaym/exec"
+  API_URL: https://script.google.com/macros/s/AKfycbwzzhD18pyECMHnCpMUkbn2vv_7Vz-WRwDAH67gPJb-qtfgabdAKePeDEcEQRUTlaym/exec
 };
